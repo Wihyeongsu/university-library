c92a945fce4b89e5b2df5554d22604b37a9e68a3
Wihyeongsu <wibrother2@gmail.com>
2025-02-14 22:54:13 +0900

    Ended User onboarding flow

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
@@ -8,10 +9,10 @@ const config = {
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
-      redisUrl: process.env.UPSTASH_REDIS_URL,
-      redisToken: process.env.UPSTASH_REDIS_TOKEN,
-      qstashUrl: process.env.QSTASH_URL,
-      qstashToekn: process.env.QSTASH_TOKEN,
+      redisUrl: process.env.UPSTASH_REDIS_URL!,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToekn: process.env.QSTASH_TOKEN!,
     },
   },
 };
